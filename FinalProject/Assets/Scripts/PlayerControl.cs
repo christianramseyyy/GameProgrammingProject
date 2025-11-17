3af1ff2877206948655cb728e26a14b91408f939
Zach <zk00958@georgiasouthern.edu>
2025-11-17 16:25:51 -0500

    Fixed issue with comet not sending player to gameover

--- FinalProject/Assets/Scripts/PlayerControl.cs
+++ FinalProject/Assets/Scripts/PlayerControl.cs
@@ -48,10 +48,11 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Alien") || collision.CompareTag("EnemyLaser") ||
-            collision.CompareTag("UFO"))
+            collision.CompareTag("UFO") || collision.CompareTag("PurpleComet"))
         {
             Debug.Log("Player collided with alien!");
             AudioSource.PlayClipAtPoint(damageSfx, transform.position);
+            audioSource.PlayOneShot(damageSfx);
 
             Destroy(gameObject);
             SceneManager.LoadScene("GameOver");
